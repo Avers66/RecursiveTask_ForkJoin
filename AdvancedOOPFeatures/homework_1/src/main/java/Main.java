9d6cfa7628fa8e95039b81939304234a79ad5038
Alexandr T <avers66@yandex.ru>
2022-10-11 22:43:21 +0500

    OOPFeatures homework1 done

--- AdvancedOOPFeatures/homework_1/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_1/src/main/java/Main.java
@@ -7,9 +7,24 @@ public class Main {
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         System.out.println(staff);
+        sortBySalaryAndAlphabet(staff);
+        System.out.println("Отсортированный список");
+        System.out.println(staff);
+        for (Employee e : staff) {
+            System.out.println(e.getSalary() + " " + e.getName());
+        }
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
         //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+
+        staff.sort((o1, o2) -> {
+            int salarySort = o1.getSalary() - o2.getSalary();
+            if (salarySort !=0) {
+                return salarySort;
+            }
+            int nameSort = o1.getName().compareTo(o2.getName());
+            return nameSort;
+        } );
     }
 }
