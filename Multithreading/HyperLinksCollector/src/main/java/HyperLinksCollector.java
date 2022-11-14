bfea735e7ac2b25150124133f082c6a773df6e43
Alexandr T <avers66@yandex.ru>
2022-11-14 11:00:55 +0500

    Multithreading. HyperLinksCollector update.

--- Multithreading/HyperLinksCollector/src/main/java/HyperLinksCollector.java
+++ Multithreading/HyperLinksCollector/src/main/java/HyperLinksCollector.java
@@ -12,8 +12,8 @@ public class HyperLinksCollector extends RecursiveTask<List<String>> {
 
     private String hyperLink;
     private int level;
-    private String root; // Домен для сканирования
-    private int recurseDepth = 40; //ограничитель глубины рекурсии
+    private String root; // Domain to scan
+    private int recurseDepth = 40; //recursion depth limiter
 
     public HyperLinksCollector(String hyperLink, int level, String root) {
         this.hyperLink = hyperLink;
@@ -31,27 +31,26 @@ public class HyperLinksCollector extends RecursiveTask<List<String>> {
         try {
             doc = Jsoup.connect(hyperLink).timeout(3000).get();
         } catch (IOException ex) {
-            System.out.println("Страница недоступна " + hyperLink); return list;} // страница недоступна
+            System.out.println("Страница недоступна " + hyperLink); return list;} // the page is unavailable
         Elements links = doc.select("a");
         for (Element e : links) {
             String hyperLinkChild = e.attr("href");
-            //if (level > recurseDepth) break; //ограничение глубины рекурсии
-            if (hyperLinkChild.indexOf(root) >= 0 && hyperLinkChild.endsWith("/")) { //гиперссылка  является дочерней
-                if (Main.set.contains(hyperLinkChild)) continue; //ссылка уже была сохранена ранее
-                Main.set.add(hyperLinkChild);
+            //if (level > recurseDepth) break; //recursion depth limiter
+            if (hyperLinkChild.indexOf(root) >= 0 && hyperLinkChild.endsWith("/")) { //hyperlink is a child
+                if (!Main.set.add(hyperLinkChild)) continue;// skip it if the link has already been saved earlier
                 try {
-                    Thread.sleep(150); //задержка перед открытием гиперссылки
+                    Thread.sleep(150); //delay before opening a hyperlink
                 } catch (InterruptedException ex) { ex.printStackTrace();}
                 HyperLinksCollector task = new HyperLinksCollector(hyperLinkChild, level + 1, root);
                 task.fork();
                 tasks.add(task);
-                //System.out.println(hyperLinkChild + " " + level); //отладка
+                //System.out.println(hyperLinkChild + " " + level); //debugging
             }
         }
         for (HyperLinksCollector item : tasks) {
             list.addAll(item.join());
         }
-        return list; //список ссылок на текущей странице
+        return list; //list of links on the current page
     }
 }
 
--- Multithreading/HyperLinksCollector/src/main/java/Main.java
+++ Multithreading/HyperLinksCollector/src/main/java/Main.java
@@ -9,48 +9,44 @@ import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-public static Set<String> set = new HashSet<>();
+    public static Set<String> set = new HashSet<>();
+    public static ForkJoinPool pool;
     public static void main(String[] args) throws IOException {
         String rootSite = "https://skillbox.ru/";
         //String rootSite = "https://lenta.ru/";
-        int level = 0; // уровень рекурсии
-        String root = rootSite; // домен
-        set.add(rootSite); //запись оригинальных ссылок
+        int level = 0; // recursion level
+        String root = rootSite; // Domain to scan
+        set.add(rootSite); //repository of original links
         long start = System.currentTimeMillis();
         HyperLinksCollector collector = new HyperLinksCollector(rootSite, level, root);
-        ForkJoinPool pool = new ForkJoinPool();
+        pool = new ForkJoinPool();
         pool.execute(collector);
-
-        do {
-            System.out.printf("******************************************\n");
-            System.out.printf("Main: Parallelism: %d\n", pool.getParallelism());
-            System.out.printf("Main: Active Threads: %d\n", pool.getActiveThreadCount());
-            System.out.printf("Main: Task Count: %d\n", pool.getQueuedTaskCount());
-            System.out.printf("Main: Steal Count: %d\n", pool.getStealCount());
-            System.out.printf("******************************************\n");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } while (!collector.isDone());
-
-       // pool.shutdown();
-
-
+        while (!collector.isDone()) {showParametersForkJoinPool();}
+        pool.shutdown();
         List<String> results;
         results = collector.join();
-
         System.out.println(results);
         System.out.println("Количество страниц " + results.size());
-
         long duration = System.currentTimeMillis() - start;
         System.out.println(duration + " ms");
-
-        Path path = Paths.get("data/PageOfSite.txt");
-
+        String strSite = rootSite.replaceAll("https?:|/+", "");
+        Path path = Paths.get("data/MapOfSite-" + strSite + ".txt");
         try{
             Files.write(path, results, Charset.defaultCharset());
         }catch (IOException ex) { ex.printStackTrace();}
+    }
+
+    public static void showParametersForkJoinPool() {
+        System.out.printf("******************************************\n");
+        System.out.printf("Main: Parallelism: %d\n", pool.getParallelism());
+        System.out.printf("Main: Active Threads: %d\n", pool.getActiveThreadCount());
+        System.out.printf("Main: Task Count: %d\n", pool.getQueuedTaskCount());
+        System.out.printf("Main: Steal Count: %d\n", pool.getStealCount());
+        System.out.printf("******************************************\n");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
